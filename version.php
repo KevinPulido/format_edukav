8a3d7f6c87016bec56611b7af8d004c6064064b0
KevinPulido <kev_pulbravo@hotmail.com>
2026-09-15 11:41:11 -0500

    feat(format_edukav): integra el resumen de la biblioteca en la vista del curso

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026090708;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026091501;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112800;        // Requires this Moodle version.
 $plugin->supported = [ 401, 500 ];
 $plugin->component = 'format_edukav';    // Full name of the plugin (used for diagnostics).
@@ -33,4 +33,5 @@ $plugin->release = "v1.0.1";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
     "format_topics" => 2022112800,
+    "local_edukav" => 2026091500,
 ];
